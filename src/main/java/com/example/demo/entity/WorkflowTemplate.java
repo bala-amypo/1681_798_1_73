712a59b44622f82c8a3119879f9cdb116372ec21
bala-amypo <bala@amypo.in>
2025-12-18 05:54:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/WorkflowTemplate.java
+++ src/main/java/com/example/demo/entity/WorkflowTemplate.java
@@ -9,5 +9,49 @@ public class WorkflowTemplate{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    private String 
+    @Column(unique=true)
+    private String templateName;
+    private String description;
+    private Integer totalLevels;
+    private Boolean active;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTemplateName() {
+        return templateName;
+    }
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Integer getTotalLevels() {
+        return totalLevels;
+    }
+    public void setTotalLevels(Integer totalLevels) {
+        this.totalLevels = totalLevels;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public user() {
+    }
+    public user(Long id, String templateName, String description, Integer totalLevels, Boolean active) {
+        this.id = id;
+        this.templateName = templateName;
+        this.description = description;
+        this.totalLevels = totalLevels;
+        this.active = active;
+    }
+     
 }
