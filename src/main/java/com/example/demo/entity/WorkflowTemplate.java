f68e734487f31b170022a5c77d8bf424acedd76a
bala-amypo <bala@amypo.in>
2025-12-18 06:34:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/WorkflowTemplate.java
+++ src/main/java/com/example/demo/entity/WorkflowTemplate.java
@@ -3,6 +3,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+
 @Entity
 public class WorkflowTemplate{
     @Id
