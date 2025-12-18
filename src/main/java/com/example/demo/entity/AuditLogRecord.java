fe7bbc3f960cefc8cf1c878697db2ec73ce40ab2
bala-amypo <bala@amypo.in>
2025-12-18 06:33:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/AuditLogRecord.java
+++ src/main/java/com/example/demo/entity/AuditLogRecord.java
@@ -1,4 +1,5 @@
 package com.example.demo.entity;
+import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,5 +8,49 @@ import jakarta.persistence.Id;
 public class WorkflowStepConfig{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Long requestId;
+    private String eventType;
+    private String details;
+    private LocalDateTime loggedAt=LocalDateTime.now();
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getRequestId() {
+        return requestId;
+    }
+    public void setRequestId(Long requestId) {
+        this.requestId = requestId;
+    }
+    public String getEventType() {
+        return eventType;
+    }
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+    public String getDetails() {
+        return details;
+    }
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    public LocalDateTime getLoggedAt() {
+        return loggedAt;
+    }
+    public void setLoggedAt(LocalDateTime loggedAt) {
+        this.loggedAt = loggedAt;
+    }
+    public user() {
+    }
+    public user(Long id, Long requestId, String eventType, String details, LocalDateTime loggedAt) {
+        this.id = id;
+        this.requestId = requestId;
+        this.eventType = eventType;
+        this.details = details;
+        this.loggedAt = loggedAt;
+    }
     
 }
