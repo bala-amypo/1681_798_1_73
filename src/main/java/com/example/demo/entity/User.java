e8ecce53484cca0c52e54c76fba9abaaaec1dd37
bala-amypo <bala@amypo.in>
2025-12-18 09:12:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,55 +1,81 @@
 package com.example.demo.entity;
+import java.util.HashSet;
+import java.util.Set;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 @Entity
-public class User{
+public class User {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
     private String password;
-    private String roles;
+    
+
+    @ManyToMany
+    @JoinTable(
+        name="user_roles",
+        joinColumns=@JoinColumn(name="user_id"),
+        inverseJoinColumns = @JoinColumn(name="role_id")
+    )
+    private Set<Role>roles=new HashSet<>();
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
-    public String getRoles() {
+
+    public Set<Role> getRoles() {
         return roles;
     }
-    public void setRoles(String roles) {
+
+    public void setRoles(Set<Role> roles) {
         this.roles = roles;
     }
+
     public User() {
     }
-    public User(Long id, String username, String email, String password, String roles) {
+
+    public User(Long id, String username, String email, String password, Set<Role> roles) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
         this.roles = roles;
     }
-
+    
 }
