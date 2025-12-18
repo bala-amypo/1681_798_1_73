eafed17125cb251d0c96c34cf5d6062b08ed1a1c
bala-amypo <bala@amypo.in>
2025-12-18 04:57:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/WorkflowTemplate.java
+++ src/main/java/com/example/demo/entity/WorkflowTemplate.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 @Entity
-public class User{
+public class WorkflowTemplate{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
