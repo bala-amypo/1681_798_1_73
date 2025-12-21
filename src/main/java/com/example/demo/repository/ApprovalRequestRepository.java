b3c4f261e4f5939c95dbdc13a95b3fa7b36d4e5a
bala-amypo <bala@amypo.in>
2025-12-21 15:24:34 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApprovalRequestRepository.java
+++ src/main/java/com/example/demo/repository/ApprovalRequestRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.entity.ApprovalRequest;
 
-public interface ApprovalRequestRepository extends JpaRepository<ApprovalRequest,Long> {
-    
+public interface ApprovalRequestRepository
+        extends JpaRepository<ApprovalRequest, Long> {
+
+    List<ApprovalRequest> findByRequesterId(Long requesterId);
 }
