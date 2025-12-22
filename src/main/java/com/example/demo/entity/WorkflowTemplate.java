6c01d2c21546b034dca37266d31b5214659c4211
bala-amypo <bala@amypo.in>
2025-12-22 08:26:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/WorkflowTemplate.java
+++ src/main/java/com/example/demo/entity/WorkflowTemplate.java
@@ -1,52 +1,68 @@
 package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
+
+import jakarta.persistence.*;
 
 @Entity
-public class WorkflowTemplate{
+@Table(name = "workflow_templates", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "templateName")
+})
+public class WorkflowTemplate {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique=true)
+
     private String templateName;
+
     private String description;
+
     private Integer totalLevels;
+
     private Boolean active;
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getTemplateName() {
         return templateName;
     }
+
     public void setTemplateName(String templateName) {
         this.templateName = templateName;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public Integer getTotalLevels() {
         return totalLevels;
     }
+
     public void setTotalLevels(Integer totalLevels) {
         this.totalLevels = totalLevels;
     }
+
     public Boolean getActive() {
         return active;
     }
+
     public void setActive(Boolean active) {
         this.active = active;
     }
+
     public WorkflowTemplate() {
     }
+
     public WorkflowTemplate(Long id, String templateName, String description, Integer totalLevels, Boolean active) {
         this.id = id;
         this.templateName = templateName;
@@ -54,5 +70,4 @@ public class WorkflowTemplate{
         this.totalLevels = totalLevels;
         this.active = active;
     }
-     
 }
