883dde25f6818d4f164314e16cb74cd5eb21fb0e
bala-amypo <bala@amypo.in>
2025-12-25 15:24:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/AuditLogRecordServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.AuditLogRecord;
+import com.example.demo.repository.AuditLogRecordRepository;
+import com.example.demo.service.AuditLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuditLogServiceImpl implements AuditLogService {
+
+    @Autowired
+    private AuditLogRecordRepository auditLogRecordRepository;
+
+    public AuditLogRecord save(AuditLogRecord record) {
+        return auditLogRecordRepository.save(record);
+    }
+
+    public List<AuditLogRecord> findByRequestId(Long requestId) {
+        return auditLogRecordRepository.findByRequestId(requestId);
+    }
+}
