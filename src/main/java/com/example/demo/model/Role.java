c30ca7905a95f1078d5729acd2194c57f1cae43f
bala-amypo <bala@amypo.in>
2025-12-25 10:41:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Role.java
@@ -0,0 +1,34 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public Set<User> getUsers() { return users; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setUsers(Set<User> users) { this.users = users; }
+
+    public Role() {}
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
