19f356dde7e652ad6e91c371af538041caa47462
bala-amypo <bala@amypo.in>
2025-12-25 10:47:10 +0000

    User commit

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.example.handling.exception;
-public class ResourceNotFoundException extends RuntimeException {
-public ResourceNotFoundException(String message) {
-super(message);
-}
-}
--- src/main/java/com/example/demo/service/ApprovalActionService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.ApprovalAction;
-
-public interface ApprovalActionService {
-
-    ApprovalAction recordAction(ApprovalAction action);
-}
