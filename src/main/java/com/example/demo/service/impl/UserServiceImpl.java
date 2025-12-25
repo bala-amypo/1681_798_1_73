0895d888ad78a937487adeed9acf06c768096a33
bala-amypo <bala@amypo.in>
2025-12-25 17:18:11 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -5,32 +5,24 @@ import com.example.demo.model.User;
 import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
 
-    public UserServiceImpl(
-            UserRepository userRepository,
-            RoleRepository roleRepository,
-            PasswordEncoder passwordEncoder) {
+    @Autowired
+    private RoleRepository roleRepository;
 
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
-    public User register(User user, String roleName) {
-
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+    public User registerUser(User user, String roleName) {
 
         Role role = roleRepository.findByName(roleName)
                 .orElseGet(() -> {
@@ -39,21 +31,15 @@ public class UserServiceImpl implements UserService {
                     return roleRepository.save(r);
                 });
 
-        user.setRoles(Collections.singleton(role));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.getRoles().add(role);
+
         return userRepository.save(user);
     }
 
     @Override
-    public User login(String usernameOrEmail, String rawPassword) {
-
-        User user = userRepository
-                .findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
                 .orElseThrow();
-
-        if (!passwordEncoder.matches(rawPassword, user.getPassword())) {
-            throw new RuntimeException("Invalid password");
-        }
-
-        return user;
     }
 }
