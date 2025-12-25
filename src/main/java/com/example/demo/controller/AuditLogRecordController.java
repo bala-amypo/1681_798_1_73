b2506f9e653668e874b925c942aa1947ab8ff681
bala-amypo <bala@amypo.in>
2025-12-25 16:05:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuditLogRecordController.java
+++ src/main/java/com/example/demo/controller/AuditLogRecordController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.AuditLogRecord;
-import com.example.demo.service.AuditLogService;
+import com.example.demo.service.AuditLogRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
