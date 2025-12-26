baf8ee86eb93dd94ba139b6545dd9922e8c01005
bala-amypo <bala@amypo.in>
2025-12-26 13:25:30 +0000

    User commit

--- src/main/java/com/example/demo/util/HibernateQueryUtil.java
+++ src/main/java/com/example/demo/util/HibernateQueryUtil.java
@@ -1,30 +1,21 @@
 package com.example.demo.util;
 
-import com.example.demo.model.ApprovalAction;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 
-@Component
 public class HibernateQueryUtil {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    private HibernateQueryUtil() {
+    }
 
-    public List<ApprovalAction> findActionsByApproverUsingCriteria(Long approverId) {
-
+    public static <T> List<T> fetchAll(EntityManager entityManager, Class<T> clazz) {
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<ApprovalAction> cq = cb.createQuery(ApprovalAction.class);
-        Root<ApprovalAction> root = cq.from(ApprovalAction.class);
-
-        cq.select(root)
-          .where(cb.equal(root.get("approverId"), approverId));
-
+        CriteriaQuery<T> cq = cb.createQuery(clazz);
+        Root<T> root = cq.from(clazz);
+        cq.select(root);
         return entityManager.createQuery(cq).getResultList();
     }
 }
