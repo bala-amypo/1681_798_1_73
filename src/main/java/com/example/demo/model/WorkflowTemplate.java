65e0789540d80187c13f08ed79a311bd59ec74f4
bala-amypo <bala@amypo.in>
2025-12-26 14:47:20 +0000

    User commit

--- src/main/java/com/example/demo/model/WorkflowTemplate.java
+++ src/main/java/com/example/demo/model/WorkflowTemplate.java
@@ -1,32 +1,59 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-
-@Entity
 public class WorkflowTemplate {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String name;       
+    private String templateName;
+    private int totalLevels;
+    private String description;
     private boolean active;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public boolean isActive() { return active; }
-    public void setActive(boolean active) { this.active = active; }
+    public String getTemplateName() {
+        return templateName;
+    }
 
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
 
+    public int getTotalLevels() {
+        return totalLevels;
+    }
+
+    public void setTotalLevels(int totalLevels) {
+        this.totalLevels = totalLevels;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
     public WorkflowTemplate() {}
 
-    public WorkflowTemplate(Long id, String name,Boolean active) {
+    public WorkflowTemplate(Long id, String templateName, int totalLevels, String description, boolean active) {
         this.id = id;
-        this.name = name;
+        this.templateName = templateName;
+        this.totalLevels = totalLevels;
+        this.description = description;
         this.active = active;
     }
 }
