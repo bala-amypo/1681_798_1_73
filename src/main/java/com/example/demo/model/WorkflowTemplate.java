14452d43d7ddca7c2ceb240579957fdd91078508
bala-amypo <bala@amypo.in>
2025-12-26 13:49:40 +0000

    User commit

--- src/main/java/com/example/demo/model/WorkflowTemplate.java
+++ src/main/java/com/example/demo/model/WorkflowTemplate.java
@@ -8,20 +8,25 @@ public class WorkflowTemplate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private Integer totalLevels;
+    private Boolean active;
+    private String description;
 
-    private String name;       // make sure `getName()` exists
-    private boolean active;
-
-    // getters & setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public boolean isActive() { return active; }
-    public void setActive(boolean active) { this.active = active; }
+    public Integer getTotalLevels() { return totalLevels; }
+    public void setTotalLevels(Integer totalLevels) { this.totalLevels = totalLevels; }
 
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
     public WorkflowTemplate() {}
 
