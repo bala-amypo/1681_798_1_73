04f542c2b904405dd0a06a607105d3799c1fd375
bala-amypo <bala@amypo.in>
2025-12-26 04:30:05 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,19 +1,29 @@
 package com.example.demo.security;
 
+import com.example.demo.model.User;
 import org.springframework.stereotype.Component;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtTokenProvider {
 
-    public String generateToken(Long id, String email, List<String> roles) {
+    public String generateToken(User user) {
+        Long id = user.getId();
+        String email = user.getEmail();
+        List<String> roles = user.getRoles().stream().map(r -> r.getName()).collect(Collectors.toList());
         return id + "-" + email + "-" + String.join(",", roles);
     }
 
     public Long getUserIdFromToken(String token) {
-        try { return Long.parseLong(token.split("-")[0]); }
-        catch(Exception e) { return null; }
+        try {
+            return Long.parseLong(token.split("-")[0]);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
-    public boolean validateToken(String token) { return token != null && token.contains("-"); }
+    public boolean validateToken(String token) {
+        return token != null && token.contains("-");
+    }
 }
