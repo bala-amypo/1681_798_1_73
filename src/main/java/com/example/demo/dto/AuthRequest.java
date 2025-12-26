f2ef93b01deff9af59f103eb47aad33afca7fcd1
bala-amypo <bala@amypo.in>
2025-12-26 03:09:53 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -5,22 +5,6 @@ public class AuthRequest {
     private String usernameOrEmail;
     private String password;
 
-    public AuthRequest() {
-    }
-
-    public String getUsernameOrEmail() {
-        return usernameOrEmail;
-    }
-
-    public void setUsernameOrEmail(String usernameOrEmail) {
-        this.usernameOrEmail = usernameOrEmail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getUsernameOrEmail() { return usernameOrEmail; }
+    public String getPassword() { return password; }
 }
