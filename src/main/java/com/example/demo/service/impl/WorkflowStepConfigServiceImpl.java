1d2bc402bae78b302552a558d59071e0dc08a465
bala-amypo <bala@amypo.in>
2025-12-27 01:24:28 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/WorkflowStepConfigServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/WorkflowStepConfigServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Service
 public class WorkflowStepConfigServiceImpl implements WorkflowStepConfigService {
-
+    @Autowired
     private final WorkflowStepConfigRepository repository;
 
     public WorkflowStepConfigServiceImpl(WorkflowStepConfigRepository repository) {
