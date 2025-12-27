16f4b05fc8ce1f5988379b1700f0252659caeb2c
bala-amypo <bala@amypo.in>
2025-12-27 04:40:54 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,12 +1,28 @@
 package com.example.demo.dto;
 
-public class AuthResponse {
-    private String token;
-    private String type = "Bearer";
+import java.util.List;
 
-    public AuthResponse(String token) { this.token = token; }
-    public String getToken() { return token; }
-    public void setToken(String token) { this.token = token; }
-    public String getType() { return type; }
-    public void setType(String type) { this.type = type; }
+public class AuthResponse {
+
+    private String token;
+    private String username;
+    private List<String> roles;
+
+    public AuthResponse(String token, String username, List<String> roles) {
+        this.token = token;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
 }
