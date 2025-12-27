fc83b28a257f97bdc0618017f8da213ce979fcfd
bala-amypo <bala@amypo.in>
2025-12-27 01:24:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuditLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuditLogServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Service
 public class AuditLogServiceImpl implements AuditLogService {
-
+    @Autowired
     private final AuditLogRecordRepository repository;
 
     public AuditLogServiceImpl(AuditLogRecordRepository repository) {
