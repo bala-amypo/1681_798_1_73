ddf427d13d9e19897d02a3143847596b17503c07
bala-amypo <bala@amypo.in>
2025-12-27 03:47:05 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuditLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuditLogServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.AuditLogRecord;
-import com.example.demo.repository.AuditLogRepository;
+import com.example.demo.repository.AuditLogReordRepository;
 import com.example.demo.service.AuditLogService;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +10,9 @@ import java.util.List;
 @Service
 public class AuditLogServiceImpl implements AuditLogService {
 
-    private final AuditLogRepository repository;
+    private final AuditLogReordRepository repository;
 
-    public AuditLogServiceImpl(AuditLogRepository repository) {
+    public AuditLogServiceImpl(AuditLogRecordRepository repository) {
         this.repository = repository;
     }
 
